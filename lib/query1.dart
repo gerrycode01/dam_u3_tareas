2983a4666a488e1de19836c9669285885fc8b837
jvm750 <jomavazquezme@ittepic.edu.mx>
2024-04-24 07:34:40 -0700

    listo

--- lib/query1.dart
+++ lib/query1.dart
@@ -1,4 +1,5 @@
 import 'package:dam_u3_practica2_tarea/controlador/conexion.dart';
+import 'package:dam_u3_practica2_tarea/vistas/Materia/crearMateria.dart';
 import 'package:flutter/material.dart';
 import 'package:dam_u3_practica2_tarea/controlador/db_materia.dart';
 import 'package:dam_u3_practica2_tarea/modelo/materia.dart';
@@ -50,50 +51,58 @@ class _Query1State extends State<Query1> {
           ),
           itemCount: materias.length,
           itemBuilder: (BuildContext context, int index) {
-            return Card(
-              elevation: 4.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  AspectRatio(
-                    aspectRatio: 2.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.pink.shade100,
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(4.0),
-                          topRight: Radius.circular(4.0),
+            return InkWell(
+              onTap: () {
+                // Navegar a la nueva pantalla de detalles pasando la materia seleccionada
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => crearMateria(),
+                ));
+              },
+              child: Card(
+                elevation: 4.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 2.0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.pink.shade100,
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        child: Icon(
+                          Icons.school,
+                          size: 48.0,
+                          color: Colors.pink.shade600,
                         ),
                       ),
-                      child: Icon(
-                        Icons.school,
-                        size: 48.0,
-                        color: Colors.pink.shade600,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        materias[index].nombre,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      materias[index].nombre,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'Profesor: ${materias[index].docente}',
+                        style: const TextStyle(fontSize: 14.0),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      'Profesor: ${materias[index].docente}',
-                      style: const TextStyle(fontSize: 14.0),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  // Aquí podrías añadir más información relevante si fuera necesario.
-                ],
+                    // Aquí podrías añadir más información relevante si fuera necesario.
+                  ],
+                ),
               ),
             );
           },
